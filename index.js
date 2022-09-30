5c16b9c5b686615685c3481dfe4e21cf254a6134
raulibanez <raulibaper@gmail.com>
2022-09-30 09:27:24 +0200

    linting to match official uppy PR

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express')
-const app = express()
-const path = require('path');
-const port = process.env.PORT
-const bodyParser = require('body-parser');
 
-const aws = require('aws-sdk');
+const app = express()
+const path = require('node:path')
+
+const port = process.env.PORT
+const bodyParser = require('body-parser')
+
+const aws = require('aws-sdk')
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  const htmlPath = path.join(__dirname, 'public', 'index.html');
-  res.sendFile(htmlPath);
+  const htmlPath = path.join(__dirname, 'public', 'index.html')
+  res.sendFile(htmlPath)
 })
 
 app.get('/drag', (req, res) => {
-  const htmlPath = path.join(__dirname, 'public', 'drag.html');
-  res.sendFile(htmlPath);
+  const htmlPath = path.join(__dirname, 'public', 'drag.html')
+  res.sendFile(htmlPath)
 })
 
 app.post('/sign-s3', (req, res) => {
-  const s3 = new aws.S3();
-  const fileName = req.body.filename;
-  const contentType = req.body.contentType;
+  const s3 = new aws.S3()
+  const fileName = req.body.filename
+  const { contentType } = req.body
   const s3Params = {
     Bucket: process.env.S3_BUCKET,
     Key: fileName,
     Expires: 60,
     ContentType: contentType,
-    ACL: 'public-read'
-  };
+    ACL: 'public-read',
+  }
 
   s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
-      console.log(err);
-      return res.end();
+    if (err) {
+      console.log(err)
+      return res.end()
     }
     const returnData = {
       url: data,
-      method: 'PUT'
-    };
-    res.write(JSON.stringify(returnData));
-    res.end();
-  });
-});
+      method: 'PUT',
+    }
+    res.write(JSON.stringify(returnData))
+    res.end()
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
